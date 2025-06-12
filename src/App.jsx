f824611e2d315b2ce922e559c42f62b1863aae8d
Jeremie BAYON <jeremie.bayon@careside.care>
2025-06-12 10:01:12 +0200

    Fix conflicts after merging jeremie and louis into main

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import SignIn from './templates/authentication/SignIn'
-<<<<<<< HEAD
 import SignUp from './templates/authentication/SignUp'
 import DisplayMenu from './templates/menu/DisplayMenu'
-// import DisplayMyReservations from './templates/reservation/DisplayMyReservations'
-// import AddReservation from './templates/reservation/AddReservation'
-// import DisplayReservations from './templates/admin/DisplayReservations'
-=======
 import SignUp from './templates/authentication/SignUp.jsx'
 import AddReservation from './templates/reservation/AddReservation.jsx'
 import DisplayMyReservations from './templates/reservation/DisplayMyReservations.jsx'
 import DisplayReservations from './templates/admin/DisplayReservations.jsx'
-// import DisplayMenu from './templates/menu/DisplayMenu'
-
->>>>>>> louis
 
 export default function App() {
   return (
@@ -22,13 +14,8 @@ export default function App() {
       <Routes>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-<<<<<<< HEAD
         <Route path="/menu" element={<DisplayMenu />} />
-        {/* <Route path="/my-reservations" element={<DisplayMyReservations />} />
-=======
-         {/* <Route path="/menu" element={<DisplayMenu />} /> */}
         <Route path="/my-reservations" element={<DisplayMyReservations />} />
->>>>>>> louis
         <Route path="/reservations/new" element={<AddReservation />} />
         <Route path="/reservations" element={<DisplayReservations />} />
       </Routes>
